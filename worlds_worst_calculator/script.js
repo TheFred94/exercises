0fe87e1482247edd276f39773cd33806decc893b
TheFred94 <frederikrlarsen@gmail.com>
2023-02-03 13:10:24 +0100

    calculation function works

--- worlds_worst_calculator/script.js
+++ worlds_worst_calculator/script.js
@@ -1,15 +1,22 @@
 "use strict";
 
-let firstNumber = document.querySelector("#firstnumber").value;
-let secondNumber = document.querySelector("#secondnumber").value;
-let operator = document.querySelector("#operator").value;
-
 // Looks for click on the calculate button and runs the read numbers funtion
-const calculate = document.querySelector("#calculate").addEventListener("click", readNumbers);
+const calculate = document.querySelector("#calculate").addEventListener("click", doMath);
 
 // Reads the numbers from the first, second and operator field
-function readNumbers() {
+function doMath() {
+  let firstNumber = Number(document.querySelector("#firstnumber").value);
+  let secondNumber = Number(document.querySelector("#secondnumber").value);
+  let operator = document.querySelector("#operator").value;
   console.log(firstNumber);
   console.log(secondNumber);
-  console.log(operator);
+  if (operator === "add") {
+    console.log(firstNumber + secondNumber);
+  } else if (operator === "sub") {
+    console.log(firstNumber - secondNumber);
+  } else if (operator === "mul") {
+    console.log(firstNumber * secondNumber);
+  } else {
+    console.log(firstNumber / secondNumber);
+  }
 }
