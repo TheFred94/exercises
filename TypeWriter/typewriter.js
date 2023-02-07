65bf3d1ad12691a602c558cbdc7008196835a483
TheFred94 <frederikrlarsen@gmail.com>
2023-02-07 13:52:01 +0100

    loops trough the lenght of the text

--- TypeWriter/typewriter.js
+++ TypeWriter/typewriter.js
@@ -2,14 +2,14 @@
 let iterator = 0;
 let maxNumberOfIterations;
 let letter;
-const typewriterText = document.getElementById("typewriter").textContent;
+const typewriterText = document.getElementById("typewriter").textContent.length;
 
 console.log(typewriterText);
 initLoop();
 
 function initLoop() {
   console.log("Loop initiated");
-  maxNumberOfIterations = 20;
+  maxNumberOfIterations = typewriterText;
 
   loop();
 }
