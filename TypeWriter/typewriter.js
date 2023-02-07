33f00e5ff3923c2ff24579290a147dbd550f4525
TheFred94 <frederikrlarsen@gmail.com>
2023-02-07 13:50:07 +0100

    loops through 20 times

--- TypeWriter/typewriter.js
+++ TypeWriter/typewriter.js
@@ -1,1 +1,23 @@
 "use strict";
+let iterator = 0;
+let maxNumberOfIterations;
+let letter;
+const typewriterText = document.getElementById("typewriter").textContent;
+
+console.log(typewriterText);
+initLoop();
+
+function initLoop() {
+  console.log("Loop initiated");
+  maxNumberOfIterations = 20;
+
+  loop();
+}
+
+function loop() {
+  console.log("loop", iterator);
+  iterator++;
+  if (iterator <= maxNumberOfIterations) {
+    setTimeout(loop, 200);
+  }
+}
