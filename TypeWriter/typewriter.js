1378a9abf0de7dcdeefee5fe444ba7280bcc936c
TheFred94 <frederikrlarsen@gmail.com>
2023-02-07 14:37:15 +0100

    one letter in the console

--- TypeWriter/typewriter.js
+++ TypeWriter/typewriter.js
@@ -1,21 +1,20 @@
 "use strict";
 let iterator = 0;
-let maxNumberOfIterations;
-let letter;
-const typewriterText = document.getElementById("typewriter").textContent.length;
 
-console.log(typewriterText);
+let letter;
+const typewriterText = document.getElementById("typewriter").textContent;
+const maxNumberOfIterations = typewriterText.length;
+
 initLoop();
 
 function initLoop() {
   console.log("Loop initiated");
-  maxNumberOfIterations = typewriterText;
 
   loop();
 }
 
 function loop() {
-  console.log("loop", iterator);
+  console.log(typewriterText[iterator]);
   iterator++;
   if (iterator <= maxNumberOfIterations) {
     setTimeout(loop, 200);
