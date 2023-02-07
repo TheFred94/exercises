619cd479fff677b2d0282b07babbc367740b6fa8
TheFred94 <frederikrlarsen@gmail.com>
2023-02-07 19:49:28 +0100

    done? already?... wow i must be getting good at this

--- Counting_Array/script.js
+++ Counting_Array/script.js
@@ -1,2 +1,23 @@
-"use strict;";
 "use strict";
+
+let arr = [];
+let iterator = 0;
+let maxNumberOfIterations;
+initLoop();
+
+function initLoop() {
+  console.log("initLoop");
+  maxNumberOfIterations = 10;
+
+  loop();
+}
+
+function loop() {
+  console.log("loop", iterator);
+  iterator++;
+  if (iterator < maxNumberOfIterations) {
+    arr.unshift(iterator);
+    setTimeout(loop, 1000);
+    console.log(arr);
+  }
+}
