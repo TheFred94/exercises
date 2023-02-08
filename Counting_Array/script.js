35c41c8f9e4a3890dfb1f9e9db25645f095b1800
TheFred94 <FRL@gyldendal.dk>
2023-02-08 08:52:37 +0100

    comments on the script

--- Counting_Array/script.js
+++ Counting_Array/script.js
@@ -14,7 +14,9 @@ function initLoop() {
 function loop() {
   console.log("loop", iterator);
   iterator++;
+  // Splice makes sure that the array is no longer than 9 chars
   arr.splice(8);
+  // Unshit takes the newly generated item and places it first in the array
   arr.unshift(iterator);
   setTimeout(loop, 1000);
   console.log(arr);
