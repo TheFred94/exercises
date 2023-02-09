352cec002c82e4dfb1e7796d8e9748f0d6104236
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 13:13:01 +0100

    fourth artwork

--- generative_art/art.js
+++ generative_art/art.js
@@ -1,16 +1,48 @@
 "use strict";
 
 const artwork1 = document.getElementById("artwork1");
+const artwork2 = document.getElementById("artwork2");
+const artwork3 = document.getElementById("artwork3");
+const artwork4 = document.getElementById("artwork4");
 
 addArt();
 
 function addArt() {
+  // First artwork
   for (let i = 100; i < 300; i += 20) {
     const box = document.createElement("div");
     box.classList.add("box");
     box.style.height = `${i}px`;
     box.style.width = `${i}px`;
     artwork1.appendChild(box);
+    console.log("box added");
+  }
+  //  Second artwork
+  for (let i = 0; i < 90; i += 10) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.rotate = `${i}deg`;
+    artwork2.appendChild(box);
+
+    console.log("box added");
+  }
+  //  Third artwork
+  for (let i = 0; i < 200; i += 10) {
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+    circle.style.height = `${i}px`;
+    circle.style.width = `${i}px`;
+    circle.classList.add("circle");
+    artwork3.appendChild(circle);
+    console.log("circle added");
+  }
+  //   Fourth artwork
+  for (let i = -90; i < 90; i += 20) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.transform = `translate(${i}px, ${i}px)`;
+    box.classList.add("box");
+    artwork4.appendChild(box);
     console.log("box added");
   }
 }
