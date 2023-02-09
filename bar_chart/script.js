9e10d25e4a012b38893df8e0b805e62c80628ba1
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 15:06:27 +0100

    Bars are added with a specific height

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const bars = document.getElementById("bars");
+
+addRandomBars();
+
+function addRandomBars() {
+  for (let i = 0; i <= 39; i++) {
+    setTimeout(() => {
+      const bar = document.createElement("div");
+      bar.classList.add("bar");
+      bars.appendChild(bar);
+      console.log(i);
+    }, 500 * i);
+    console.log("bar added");
+  }
+}
