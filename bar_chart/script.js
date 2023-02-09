5cc5e19e2be0eb40967a6ee2721e1b05f339b511
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 15:51:02 +0100

    Random numbers are generated and the numbers are unshifted to the beginning of the array

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -1,17 +1,30 @@
 "use strict";
 
 const bars = document.getElementById("bars");
+const numbers = [];
 
-addRandomBars();
+addRandomNumber();
+
+function addRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 101);
+  numbers.unshift(randomNumber);
+  numbers.splice(40);
+  console.log(numbers);
+}
 
 function addRandomBars() {
-  for (let i = 0; i <= 39; i++) {
+  for (let i = 0; i <= 40; i++) {
     setTimeout(() => {
       const bar = document.createElement("div");
       bar.classList.add("bar");
+      bar.style.height = numbers[0] + "px";
       bars.appendChild(bar);
+      addRandomNumber();
       console.log(i);
     }, 500 * i);
     console.log("bar added");
+    numbers.push(Math.floor(Math.random()) * 41);
   }
 }
+
+addRandomBars();
