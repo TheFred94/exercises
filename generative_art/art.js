bd827a1efc3a1904eb49ab81e3938ae8b43d2c21
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 12:01:05 +0100

    1st artwork works

--- generative_art/art.js
+++ generative_art/art.js
@@ -5,10 +5,11 @@ const artwork1 = document.getElementById("artwork1");
 addArt();
 
 function addArt() {
-  for (let box = 0; box < 20; box++) {
-    let box = document.createElement("div");
-    box.innerHTML = "";
+  for (let i = 100; i < 300; i += 20) {
+    const box = document.createElement("div");
     box.classList.add("box");
+    box.style.height = `${i}px`;
+    box.style.width = `${i}px`;
     artwork1.appendChild(box);
     console.log("box added");
   }
