9f37e5d4c3c78f1846a381c98a02d2341da969e5
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 11:43:21 +0100

    adds 20 boxes

--- generative_art/art.js
+++ generative_art/art.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const artwork1 = document.getElementById("artwork1");
-const box = document.createElement("div");
 
 addArt();
 
 function addArt() {
-  box.innerHTML = "";
-  box.classList.add("box");
-  artwork1.appendChild(box);
-  console.log("box added");
+  for (let box = 0; box < 20; box++) {
+    let box = document.createElement("div");
+    box.innerHTML = "";
+    box.classList.add("box");
+    artwork1.appendChild(box);
+    console.log("box added");
+  }
 }
