40c32ce79f7a3f065d6868d3d06c2d9e49821e0b
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 10:49:06 +0100

    buttons for the lols

--- for_loop_exercise/script.js
+++ for_loop_exercise/script.js
@@ -1,39 +1,68 @@
 "use strict";
 
-// document.addEventListener("DOMContentLoaded").button.addEventListener("click");
+document.addEventListener("DOMContentLoaded", listeners);
+
+function listeners() {
+  const Btn1 = document.getElementById("1");
+  const Btn2 = document.getElementById("2");
+  const Btn3 = document.getElementById("3");
+  const Btn4 = document.getElementById("4");
+  const Btn5 = document.getElementById("5");
+  const Btn6 = document.getElementById("6");
+  const Btn7 = document.getElementById("7");
+  Btn1.addEventListener("click", zeroToTen);
+  Btn2.addEventListener("click", oneToTen);
+  Btn3.addEventListener("click", tenToZero);
+  Btn4.addEventListener("click", oneToNineteen);
+  Btn5.addEventListener("click", bigNumber);
+  Btn6.addEventListener("click", threeIncrements);
+  Btn7.addEventListener("click", downFromAhundred);
+}
 
 // Counts from 0 - 9
-for (let counter = 0; counter < 10; counter++) {
-  console.log(counter);
+function zeroToTen() {
+  for (let counter = 0; counter < 10; counter++) {
+    console.log(counter);
+  }
 }
 
 // Counts from 1 - 10
-for (let counter = 1; counter < 11; counter++) {
-  console.log(counter);
+function oneToTen() {
+  for (let counter = 1; counter < 11; counter++) {
+    console.log(counter);
+  }
 }
 
 // Counts from 10 to 0 and the lifts off
-for (let counter = 10; counter > -1; counter--) {
-  console.log(counter);
+function tenToZero() {
+  for (let counter = 10; counter > -1; counter--) {
+    console.log(counter);
+  }
+  console.log("liftoff");
 }
-console.log("liftoff");
-
 // Counts from 1 - 19 in uneven numbers
-for (let counter = 1; counter < 20; counter += 2) {
-  console.log(counter);
+function oneToNineteen() {
+  for (let counter = 1; counter < 20; counter += 2) {
+    console.log(counter);
+  }
 }
-
 // Counts to 16777216 and doubles each time
-for (let counter = 1; counter < 16777216; counter *= 2) {
-  console.log(counter);
+function bigNumber() {
+  for (let counter = 1; counter <= 16777216; counter *= 2) {
+    console.log(counter);
+  }
 }
 
 // Counts from 111 to 138 in increments of 3
-for (let counter = 111; counter < 139; counter += 3) {
-  console.log(counter);
+function threeIncrements() {
+  for (let counter = 111; counter < 139; counter += 3) {
+    console.log(counter);
+  }
 }
 
 // Counts down from 100 in increments of 10
-for (let counter = 100; counter > 0; counter -= 10) {
-  console.log(counter);
+function downFromAhundred() {
+  for (let counter = 100; counter > 0; counter -= 10) {
+    console.log(counter);
+  }
 }
