3472a3823d52efca444d195da113e4dbc020fed4
TheFred94 <frederikrlarsen@gmail.com>
2023-02-09 16:02:15 +0100

    works as intended

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -8,19 +8,24 @@ addRandomNumber();
 function addRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 101);
   numbers.unshift(randomNumber);
-  numbers.splice(40);
+  if (numbers.lenght > 40) {
+    numbers.pop();
+  }
   console.log(numbers);
 }
 
 function addRandomBars() {
-  for (let i = 0; i <= 40; i++) {
+  for (let i = 0; i <= 200; i++) {
     setTimeout(() => {
       const bar = document.createElement("div");
       bar.classList.add("bar");
-      bar.style.height = numbers[0] + "px";
+      bar.style.height = numbers[0] + "%";
       bars.appendChild(bar);
       addRandomNumber();
       console.log(i);
+      if (bars.children.length > 40) {
+        bars.removeChild(bars.firstChild);
+      }
     }, 500 * i);
     console.log("bar added");
     numbers.push(Math.floor(Math.random()) * 41);
