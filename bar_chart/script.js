c9b31dad03e25b3a114b8a6eed7f8beb57691b6b
TheFred94 <frederikrlarsen@gmail.com>
2023-02-10 10:58:21 +0100

    manually modified the array to check if it works on load

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const bars = document.getElementById("bars");
-const numbers = [72, 38, 94, 2, 93, 100, 56, 65, 43, 11, 14, 77, 39, 90, 99, 71, 46, 36, 61, 87, 23, 80, 30, 15, 42, 66, 20, 95, 32, 70, 12, 92];
+const numbers = [1, 38, 94, 2, 93, 100, 56, 65, 43, 11, 14, 77, 39, 90, 99, 71, 46, 36, 61, 87, 23, 80, 30, 15, 42, 66, 20, 95, 32, 70, 12, 92];
 const barElements = document.querySelectorAll(".bar");
 
 updateBars();
