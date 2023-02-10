fc63661c77b836549837024a23bd389a44473d69
TheFred94 <frederikrlarsen@gmail.com>
2023-02-10 10:48:20 +0100

    Further commenting on the script

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -5,7 +5,8 @@ const numbers = [72, 38, 94, 2, 93, 100, 56, 65, 43, 11, 14, 77, 39, 90, 99, 71,
 const barElements = document.querySelectorAll(".bar");
 
 updateBars();
-
+// Continuously generates a random number between 0 and 100 and adds it to the array
+// while removing the first number
 // Takes the value from the predefined array and show it as height % on load
 function updateBars() {
   for (let i = 0; i < barElements.length; i++) {
@@ -14,6 +15,7 @@ function updateBars() {
 }
 
 // Every second the data updates, generates a new number and
+// While removing the first number in the array
 setInterval(() => {
   const randomNumber = Math.floor(Math.random() * 100);
   numbers.shift();
