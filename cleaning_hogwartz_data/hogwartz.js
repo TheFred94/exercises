d30f352f205cf55e87ea4137763a56d9b38e6721
TheFred94 <frederikrlarsen@gmail.com>
2023-02-10 15:32:45 +0100

    data loads only full names

--- cleaning_hogwartz_data/hogwartz.js
+++ cleaning_hogwartz_data/hogwartz.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const studentDataUrl = "https://petlatkea.dk/2021/hogwarts/students.json";
-let allStudents = [];
+const allStudents = [];
 let students;
 
 const Student = {
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", loadPage);
 
 function loadPage() {
   console.log("Page loaded");
-  getData(studentDataUrl);
+  loadJSON();
 }
 
 async function getData(studentDataUrl) {
@@ -26,6 +26,41 @@ async function getData(studentDataUrl) {
   showListOfStudents();
 }
 
+function loadJSON() {
+  fetch(studentDataUrl)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      prepareObjects(jsonData);
+    });
+}
+
 function showListOfStudents() {
   console.log(students);
+  prepareObjects(jsonData);
+}
+
+function prepareObjects(jsonData) {
+  jsonData.forEach((jsonObject) => {
+    const studentCard = Object.create(Student);
+    // const fullName = jsonObject.fullname.split(" ");
+
+    studentCard.firstname = jsonObject.fullname;
+
+    allStudents.push(studentCard);
+  });
+  displayList();
+}
+
+function displayList() {
+  document.querySelector("#list tbody").innterHTML = "";
+
+  allStudents.forEach(displayStudent);
+}
+
+function displayStudent(studentCard) {
+  const clone = document.querySelector("template#student").content.cloneNode(true);
+
+  clone.querySelector("[data-field=firstname]").textContent = studentCard.firstname;
+
+  document.querySelector("#list tbody").appendChild(clone);
 }
