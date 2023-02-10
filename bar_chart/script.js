ec0fcf06ddf5bc89eeb5183d6f82dd1d0e4a1ec7
TheFred94 <frederikrlarsen@gmail.com>
2023-02-10 09:01:20 +0100

    32 divs in html, removes and adds but very janky

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -39,7 +39,7 @@ function addRandomBars() {
       } else {
         bars.removeChild(bars.firstChild);
       }
-    }, 1000 * i);
+    }, 300 * i);
     console.log("bar added");
     numbers.push(Math.floor(Math.random()) * 100);
   }
