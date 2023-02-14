f32ad311d9c5d780e3c21cf8debc3b18aa3d75a7
TheFred94 <frederikrlarsen@gmail.com>
2023-02-14 10:58:06 +0100

    Sum exercises done

--- functions_and_return_values/returnValues.js
+++ functions_and_return_values/returnValues.js
@@ -1,1 +1,44 @@
 "use strict";
+// !Return Values experiment #1
+// let result = greeting("Peter");
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// console.log(result);
+
+//! Return Values experiment #2.1
+// ? The function os just called greeting("any name")
+// * Expected result = Hello Peter
+// let result = greeting("Peter");
+
+// greeting("any name");
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// console.log(result);
+
+//! Return Values experiment #2.2
+// ? The function is called in a console.log like so: console.log(greeting("any name"));
+// * Expected result = Hello any name
+// let result = greeting("Peter");
+
+// greeting("any name");
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// console.log(greeting("any name"));
+
+// ! Return Values experiment #2.3
+// ? The function is called without any parameter
+// * Expected result = firstName is not defined
+// let result = greeting("Peter");
+
+// greeting("any name");
+
+// function greeting() {
+//   return `Hello ${firstName}`;
+// }
+// console.log(greeting("any name"));
