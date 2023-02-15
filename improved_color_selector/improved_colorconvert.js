76d3430148f55a586e2a16c4ab804c6c5a0f8f34
TheFred94 <frederikrlarsen@gmail.com>
2023-02-15 22:00:08 +0100

    It works as it's supposed to

--- improved_color_selector/improved_colorconvert.js
+++ improved_color_selector/improved_colorconvert.js
@@ -21,7 +21,9 @@ function handleUserInput() {
 
   // ? Because they are returned in the "convertHEXToRGB" as an object we delcare r, g, b with rgbValues infront
   let cssColor = convertRGBToCSS(rgbValues.r, rgbValues.g, rgbValues.b);
+  let hslValues = convertRGBToHSL(rgbValues.r, rgbValues.g, rgbValues.b);
   showRGB(cssColor);
+  showHSL(hslValues.h, hslValues.s, hslValues.l);
 }
 
 function getUserColor() {
@@ -41,6 +43,44 @@ function convertRGBToCSS(r, g, b) {
   return cssString;
 }
 
+function convertRGBToHSL(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let h, s, l;
+
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  if (max === min) {
+    h = 0;
+  } else if (max === r) {
+    h = 60 * (0 + (g - b) / (max - min));
+  } else if (max === g) {
+    h = 60 * (2 + (b - r) / (max - min));
+  } else if (max === b) {
+    h = 60 * (4 + (r - g) / (max - min));
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+  // multiply s and l by 100 to get the value in percent, rather than [0,1]
+  s *= 100;
+  l *= 100;
+
+  return { h, s, l };
+}
+
 function showHEX(userColor) {
   document.querySelector("#hex").textContent = userColor;
   document.querySelector("section").style.backgroundColor = userColor;
@@ -48,4 +88,8 @@ function showHEX(userColor) {
 
 function showRGB(cssRGBValues) {
   document.querySelector("#rgb").textContent = `${cssRGBValues}`;
+}
+
+function showHSL(h, s, l) {
+  document.querySelector("#hsl").textContent = `${h.toFixed(0)}, ${s.toFixed(0)}%, ${l.toFixed(0)}%`;
 }
