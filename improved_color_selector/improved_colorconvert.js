e2003501d87081f1af50df7943cc566973e5bf4d
TheFred94 <frederikrlarsen@gmail.com>
2023-02-15 18:26:47 +0100

    jesus f**** christ

--- improved_color_selector/improved_colorconvert.js
+++ improved_color_selector/improved_colorconvert.js
@@ -18,7 +18,7 @@ function getUserColor() {
 // Stores the getUserColor function in a variable
 let userColor = getUserColor();
 
-// Creates a variable that stores the convertHEXToRGB function and parses on the value from getUserColor as the  "userColor" color variable
+// Creates a variable that stores the convertHEXToRGB function and parses on the value from getUserColor as the "userColor" color variable
 let fromHexToRgb = convertHEXToRGB(userColor);
 console.log(userColor);
 
@@ -35,35 +35,78 @@ function convertHEXToRGB(hex) {
   return { r, g, b };
 }
 
-// let hexToRgbConverted = convertRGBToHex();
-
-// let cssString = convertRGBToCSS(hexToRgbConverted);
-// // Converts the RGB color to a CSS usable string for styling eg. (255, 255, 255)
-// console.log(cssString);
-function convertRGBToCSS({ r, g, b }) {
-  let rgbObject = { r, g, b };
-  let cssString = `rgb(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b})`;
-
-  return cssString;
+// Converts the RGB to CSS
+function convertRGBToCSS(rgb) {
+  return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
 }
 
-// Not sure is this function is needed since the user input is a HEX
-function convertRGBToHex() {}
+// Converts the RGB to HEX
+function convertRGBToHex(rgb) {
+  let rHex = rgb.r.toString(16).padStart(2, "0");
+  let gHex = rgb.g.toString(16).padStart(2, "0");
+  let bHex = rgb.b.toString(16).padStart(2, "0");
+
+  return `#${rHex}${gHex}${bHex}`;
+}
 
 // Converts the RGB to HSL
-function convertRGBToHSL() {}
+function convertRGBToHSL(rgb) {
+  let r = rgb.r / 255;
+  let g = rgb.g / 255;
+  let b = rgb.b / 255;
+
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+  let delta = max - min;
+
+  let h, s, l;
+
+  if (delta === 0) {
+    h = 0;
+  } else if (max === r) {
+    h = ((g - b) / delta) % 6;
+  } else if (max === g) {
+    h = (b - r) / delta + 2;
+  } else {
+    h = (r - g) / delta + 4;
+  }
+
+  h = Math.round(h * 60);
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (max + min) / 2;
+
+  if (delta === 0) {
+    s = 0;
+  } else {
+    s = delta / (1 - Math.abs(2 * l - 1));
+  }
+
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  return { h, s, l };
+}
 
 // Shows the HSL
-function showHSL() {}
+function showHSL(hsl) {
+  console.log(`HSL(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`);
+}
 
 // Shows the RGB
-function showRGB() {}
+function showRGB(rgb) {
+  console.log(`RGB(${rgb.r}, ${rgb.g}, ${rgb.b})`);
+}
 
 // Shows the HEX
-function showHEX() {}
+function showHEX(hex) {
+  document.querySelector("#hex").textContent = "hex";
+  console.log(hex);
+}
 
 // Shows the colors in a box in the CSS
-function showCSS() {}
-
-// Not sure what this should do yet
-function showSelectedUserColor() {}
+function showCSS(css) {
+  document.querySelector("section").style.backgroundColor = "red";
+}
