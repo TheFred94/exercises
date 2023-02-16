6ef03d7f80bbf6fa1262b6743ae8b201adc07a69
TheFred94 <frederikrlarsen@gmail.com>
2023-02-16 11:36:34 +0100

    first commit

---
+++ animalbase_filtering/animalbase.js
@@ -0,0 +1,74 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let allAnimals = [];
+
+// The prototype for all animals: 
+const Animal = {
+    name: "",
+    desc: "-unknown animal-",
+    type: "",
+    age: 0
+};
+
+function start( ) {
+    console.log("ready");
+
+    // TODO: Add event-listeners to filter and sort buttons
+
+    loadJSON();
+}
+
+
+async function loadJSON() {
+    const response = await fetch("animals.json");
+    const jsonData = await response.json();
+    
+    // when loaded, prepare data objects
+    prepareObjects( jsonData );
+}
+
+function prepareObjects( jsonData ) {
+    allAnimals = jsonData.map( preapareObject );
+
+    // TODO: This might not be the function we want to call first
+    displayList(allAnimals);
+}
+
+function preapareObject( jsonObject ) {
+    const animal = Object.create(Animal);
+    
+    const texts = jsonObject.fullname.split(" ");
+    animal.name = texts[0];
+    animal.desc = texts[2];
+    animal.type = texts[3];
+    animal.age = jsonObject.age;
+
+    return animal;
+}
+
+
+function displayList(animals) {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+
+    // build a new list
+    animals.forEach( displayAnimal );
+}
+
+function displayAnimal( animal ) {
+    // create clone
+    const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+    // set clone data
+    clone.querySelector("[data-field=name]").textContent = animal.name;
+    clone.querySelector("[data-field=desc]").textContent = animal.desc;
+    clone.querySelector("[data-field=type]").textContent = animal.type;
+    clone.querySelector("[data-field=age]").textContent = animal.age;
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild( clone );
+}
+
+
--- functions_and_parameters_exercises/script.js
+++ functions_and_parameters_exercises/script.js
@@ -66,58 +66,27 @@
 //   }
 // }
 
-const person3 = {
-  firstName: "Harry",
-  lastName: "Potter",
-  hired: false,
-};
+const studentDataUrl = "https://petlatkea.dk/2021/hogwarts/students.json";
 
-const person4 = {
-  firstName: "Fred",
-  lastName: "Weasly",
-  hired: false,
-};
+init();
 
-function hire(person) {
-  person.hired = true;
+function init() {
+  console.log("init");
+  // Prepares the function loadJSON functions with the parameters
+  loadJSON(studentDataUrl, prepareData);
 }
 
-function fire(person) {
-  person.hired = false;
+// The loadJSON gets the studentDataUrl and defines a parameter called url which holds that value
+async function loadJSON(url, callback) {
+  // This const fetches the url (studentDataUrl) and stores it as "result"
+  const result = await fetch(url);
+  // Then it uses that result and stores it as a "data", which is used as a parameter for the callback function
+  const data = await result.json();
+  callback(data);
 }
 
-function fireOrHire(action, person) {
-  action(person);
+function prepareData(data) {
+  console.log("prepare" + data);
+
+  console.table(data);
 }
-
-fireOrHire(hire, person4);
-
-fireOrHire(fire, person3);
-
-console.log(person3);
-console.log(person4);
-
-// const studentDataUrl = "https://petlatkea.dk/2021/hogwarts/students.json";
-
-// init();
-
-// function init() {
-//   console.log("init");
-//   // Prepares the function loadJSON functions with the parameters
-//   loadJSON(studentDataUrl, prepareData);
-// }
-
-// // The loadJSON gets the studentDataUrl and defines a parameter called url which holds that value
-// async function loadJSON(url, callback) {
-//   // This const fetches the url (studentDataUrl) and stores it as "result"
-//   const result = await fetch(url);
-//   // Then it uses that result and stores it as a "data", which is used as a parameter for the callback function
-//   const data = await result.json();
-//   callback(data);
-// }
-
-// function prepareData(data) {
-//   console.log("prepare" + data);
-
-//   console.table(data);
-// }
